0763611c1a9175dd320347ff8a29e4747381a502
Aiken Cairncross <acairncross@gmail.com>
2021-03-07 14:02:38 +0000

    Add ODDRX1F primitive

--- src/ECP5.hs
+++ src/ECP5.hs
@@ -1,6 +1,7 @@
-module ECP5 (ecp5pll) where
+module ECP5 (ecp5pll, oddrx1) where
 
 import Clash.Clocks (Clocks (..))
+import Clash.Explicit.Signal (unsafeSynchronizer)
 import Clash.Prelude
 
 import Clash.Annotations.Primitive (Primitive (InlinePrimitive), HDL (Verilog, VHDL))
@@ -28,3 +29,31 @@ ecp5pll !_ = knownDomain @domIn `seq` knownDomain @domOut `seq` clocks
     }
   ]
   |]) #-}
+
+oddrx1
+  :: forall domIn domOut
+   . KnownDomain domIn
+  => KnownDomain domOut
+  => DomainPeriod domIn ~ (2 * DomainPeriod domOut)
+  => Signal domIn Bit
+  -> Signal domIn Bit
+  -> Clock domIn
+  -> Reset domIn
+  -> Signal domOut Bit
+oddrx1 x1 y1 !_ !_ =
+  let x2 = unsafeSynchronizer (clockGen @domIn) (clockGen @domOut) x1
+      y2 = unsafeSynchronizer (clockGen @domIn) (clockGen @domOut) y1
+  in withClockResetEnable (clockGen @domOut) (resetGen @domOut) enableGen $
+      mealyB mergeT False (x2, y2)
+  where
+    mergeT b (x, y) = (not b, if b then y else x)
+{-# NOINLINE oddrx1 #-}
+{-# ANN oddrx1 (InlinePrimitive [Verilog] $ unindent [I.i|
+  [ { "BlackBox" :
+      { "name" : "ECP5.oddrx1"
+      , "kind" : "Declaration"
+      , "template" : "ODDRX1F(~ARG[3], ~ARG[4], ~ARG[6], ~ARG[5], ~RESULT);"
+      }
+    }
+  ]
+|]) #-}
