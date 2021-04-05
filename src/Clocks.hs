77bb2dbe5b747f147f38e76ff5f0944857f78e8f
Aiken Cairncross <acairncross@gmail.com>
2021-04-05 15:45:14 +0100

    Add too many clock domains

--- src/Clocks.hs
+++ src/Clocks.hs
@@ -12,8 +12,17 @@ createDomain vXilinxSystem{vName="Dom25", vPeriod=hzToPeriod 25_000_000}
 createDomain vXilinxSystem{vName="Dom125", vPeriod=hzToPeriod 125_000_000}
 createDomain vXilinxSystem{vName="Dom250", vPeriod=hzToPeriod 250_000_000}
 
-createDomain vXilinxSystem{vName="Dom60", vPeriod=hzToPeriod 60_000_000}
-createDomain vXilinxSystem{vName="Dom300", vPeriod=hzToPeriod 300_000_000}
+createDomain vXilinxSystem{vName="Dom60", vPeriod=hzToPeriod 62_500_000}
+createDomain vXilinxSystem{vName="Dom300", vPeriod=hzToPeriod 312_500_000}
+createDomain vXilinxSystem{vName="Dom600", vPeriod=hzToPeriod 625_000_000}
+
+createDomain vXilinxSystem{vName="Dom80", vPeriod=hzToPeriod 80_000_000}
+createDomain vXilinxSystem{vName="Dom400", vPeriod=hzToPeriod 400_000_000}
+createDomain vXilinxSystem{vName="Dom800", vPeriod=hzToPeriod 800_000_000}
+
+createDomain vXilinxSystem{vName="Dom65", vPeriod=hzToPeriod 65_000_000}
+createDomain vXilinxSystem{vName="Dom325", vPeriod=hzToPeriod 325_000_000}
+createDomain vXilinxSystem{vName="Dom650", vPeriod=hzToPeriod 650_000_000}
 
 -- Divide 1s by rate - type level version of hzToPeriod
 type HzToPeriod (rate :: Nat) = Seconds 1 `Div` rate
